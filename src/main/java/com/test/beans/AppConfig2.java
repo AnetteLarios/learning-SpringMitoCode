4df937c6c6367ff836e9b3434fbf95aca9ec5706
SpectrumByte <paola121103@gmail.com>
2023-07-04 13:20:28 -0600

    Inyecting objects

--- src/main/java/com/test/beans/AppConfig2.java
+++ src/main/java/com/test/beans/AppConfig2.java
@@ -1,5 +1,12 @@
 package com.test.beans;
 
-public class AppConfig2 {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class AppConfig2 {
+	@Bean
+	public World mars() {
+		return new World();
+	}
 }
--- src/main/java/com/test/beans/Human.java
+++ src/main/java/com/test/beans/Human.java
@@ -1,5 +1,56 @@
 package com.test.beans;
 
 public class Human {
-
+	
+	private int id;
+	private String name;
+	private String nickname;
+	private Country country;
+	
+	/*public Human(int id, String name, String nickname) {
+		this.id = id;
+		this.name = name;
+		this.nickname = nickname;
+	}
+	*/
+	/*
+	public Human(int id) {
+		this.id = id;
+	}
+	
+	public Human(String nickname) {
+		this.nickname = nickname;
+	}
+	*/
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public Country getCountry() {
+		return country;
+	}
+	
+	public void setCountry(Country country) {
+		this.country = country;
+	}
 }
